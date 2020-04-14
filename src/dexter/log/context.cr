a82022c3e73e2e8c7e416615383715b924141e7d
Paul Smith <paulcsmith0218@gmail.com>
2020-04-13 21:58:57 -0400

    Add Array(ContextValue) as an option

--- src/dexter/log/context.cr
+++ src/dexter/log/context.cr
@@ -1,9 +1,9 @@
 class Log::Context
   alias ContextValue = Bool | Float32 | Float64 | Int32 | Int64 | String | Time
 
-  def to_h : Hash(String, ContextValue)
+  def to_h : Hash(String, ContextValue | Array(ContextValue))
     as_h.transform_values do |v|
-      v.raw.as?(ContextValue)
+      v.raw.as?(ContextValue) || v.raw.as?(Array(ContextValue))
     end.compact
   end
 end
