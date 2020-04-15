1e7967f9ab5482f8368eb6f49a4cfea26f95e479
Paul Smith <paulcsmith0218@gmail.com>
2020-04-15 19:29:12 -0400

    Refactor the JSONLogFormatter

--- src/dexter/json_log_formatter.cr
+++ src/dexter/json_log_formatter.cr
@@ -3,21 +3,31 @@ require "json"
 module Dexter
   struct JSONLogFormatter < BaseFormatter
     def call
-      data = {
-        "severity"  => entry.severity.to_s,
-        "source"    => entry.source,
-        "timestamp" => entry.timestamp,
-        "message"   => entry.message,
-      }
-        .merge(entry.context.as_h)
-      if ex = entry.exception
-        data = data.merge({"error" => {"class" => ex.class.name, "message" => ex.message, "backtrace" => ex.backtrace?}})
+      data = default_data.merge(entry.context.as_h)
+
+      exception_data.try do |exception_data_|
+        data = data.merge(exception_data_)
       end
 
       data
         .compact
         .reject { |_k, v| v.nil? || v.to_s.try(&.empty?) }
         .to_json(io)
+    end
+
+    private def default_data
+      {
+        "severity"  => entry.severity.to_s,
+        "source"    => entry.source,
+        "timestamp" => entry.timestamp,
+        "message"   => entry.message,
+      }
+    end
+
+    private def exception_data
+      entry.exception.try do |ex|
+        {"error" => {"class" => ex.class.name, "message" => ex.message, "backtrace" => ex.backtrace?}}
+      end
     end
   end
 end
