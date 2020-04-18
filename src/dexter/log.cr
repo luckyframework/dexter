a637b7b8e1fabed3926877f3b627a48db043da95
Paul Smith <paulcsmith0218@gmail.com>
2020-04-18 00:07:44 -0400

    Add Log.dexter to completely namespace dexter
    This way we can almost completely avoid modifying Log. We just add a
    `dexter` method.

--- src/dexter/log.cr
+++ src/dexter/log.cr
@@ -4,43 +4,49 @@ require "./base_formatter"
 require "./json_log_formatter"
 
 class Log
-  SEVERITY_MAP = {
-    debug:   Severity::Debug,
-    verbose: Severity::Verbose,
-    info:    Severity::Info,
-    warn:    Severity::Warning,
-    error:   Severity::Error,
-    fatal:   Severity::Fatal,
-  }
+  def dexter : Dexter
+    Dexter.new(self)
+  end
 
-  {% for method, severity in SEVERITY_MAP %}
-    # Logs key/value data in the Log::Context under the 'local' leu
-    #
-    # ```crystal
-    # Log.{{ method.id }} ->{ {path: "/comments", status: 200 }}
-    # ```
-    #
-    # You can also pass an exception:
-    #
-    # ```crystal
-    # Log.{{ method.id }}(exception) ->{ { query: "SELECT *" } }
-    # ```
-    def {{method.id}}(exception : Exception?, proc : Proc)
-      return unless backend = @backend
-      severity = Severity.new({{severity}})
-      return unless level <= severity
+  struct Dexter
+    SEVERITY_MAP = {
+      debug:   Severity::Debug,
+      verbose: Severity::Verbose,
+      info:    Severity::Info,
+      warn:    Severity::Warning,
+      error:   Severity::Error,
+      fatal:   Severity::Fatal,
+    }
 
-      proc_result = proc.call
+    getter log
 
-      Log.with_context do
-        Log.context.set(local: proc_result)
-        {{method.id}}(exception: exception) { "" }
+    def initialize(@log : ::Log)
+    end
+
+    {% for method, severity in SEVERITY_MAP %}
+      # Logs key/value data in the Log::Context under the 'local' leu
+      #
+      # ```crystal
+      # Log.dexter.{{ method.id }} ->{ {path: "/comments", status: 200 }}
+      # ```
+      #
+      # You can also pass an exception:
+      #
+      # ```crystal
+      # Log.dexter.{{ method.id }}(exception) ->{ { query: "SELECT *" } }
+      # ```
+      def {{method.id}}(*, exception : Exception? = nil)
+        return unless backend = log.backend
+        severity = Severity.new({{severity}})
+        return unless log.level <= severity
+
+        block_result = yield
+
+        Log.with_context do
+          Log.context.set(local: block_result)
+          log.{{method.id}}(exception: exception) { "" }
+        end
       end
-    end
-
-    # :nodoc:
-    def {{method.id}}(proc : Proc)
-      {{ method.id }}(exception: nil, proc: proc)
-    end
-  {% end %}
+    {% end %}
+  end
 end
