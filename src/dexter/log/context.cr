29628954971f78a5208c288bf0e1fb69addcdfa9
Brian J. Cardiff <bcardiff@gmail.com>
2020-05-14 18:29:33 -0300

    Drop Log::Context unneeded extensions

--- src/dexter/log/context.cr
+++ src/dexter/log/context.cr
@@ -1,11 +1,14 @@
-class Log::Context
-  def to_json(builder : JSON::Builder) : Nil
-    @raw.to_json builder
-  end
+{% if compare_versions(Crystal::VERSION, "0.35.0-0") >= 0 %}
+{% else %}
+  class Log::Context
+    def to_json(builder : JSON::Builder) : Nil
+      @raw.to_json builder
+    end
 
-  # TODO: Remove this once https://github.com/crystal-lang/crystal/pull/9104 is merged
-  # and released
-  def initialize(raw : Nil)
-    @raw = ""
+    # TODO: Remove this once https://github.com/crystal-lang/crystal/pull/9104 is merged
+    # and released
+    def initialize(raw : Nil)
+      @raw = ""
+    end
   end
-end
+{% end %}
